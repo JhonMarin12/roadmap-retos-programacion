be89cdfd8809fc65a725d999949ffa16a4e81c18
Jhon <jmarini@unal.edu.co>
2024-04-03 22:45:23 -0500

    #00-python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jhon_marin12.py
@@ -0,0 +1,28 @@
+# https://www.python.org/
+"""
+comentario multilinea
+https://www.python.org/
+"""
+
+#Creacion de variables
+
+mi_variable = 12
+CONSTANTE = 'Jhon' #Convencion para constantes
+
+#Datos primitivos
+enteros = 12
+flotante = 3.1416
+cadenas = "Esto es una cadena"
+is_bool = True #Crear variables booleanos como preguntas
+
+#otros datos
+lista = ['hola', 23, 2.7, 'a', ["lista dentro de lista", 1]]
+tupla = (1, 2, 3 , 'Jhon')
+diccionario = {
+    'nombre' : 'Jhon',
+    'edad' : 23,
+    'estatura' : 1.67
+}
+sets = {1, 2, 3}
+
+print("Hola, Python")
